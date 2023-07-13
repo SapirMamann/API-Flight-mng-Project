2d31d7d49b063befa123dbe4156befbad5449c2c
sapir mamann <sapir1999@outlook.com>
2023-07-13 14:12:05 +0300

    Added pass functions in logics

--- flightsite/flightapp/logics/flight.py
+++ flightsite/flightapp/logics/flight.py
@@ -13,3 +13,14 @@ class FlightLogic():
 
     def get_by_params(self, origin_country):
         return self.dal.get_by_params(origin_country)
+    #needs to add destination and departure time
+
+
+    def get_arrival_flights(self, destination_country):
+        pass
+    #that lands in the given country
+
+
+    def get_departure_flights(self, origin_country):
+        pass
+    #that take off from the given country
--- flightsite/flightapp/logics/ticket.py
+++ flightsite/flightapp/logics/ticket.py
@@ -10,3 +10,7 @@ class TicketLogic():
         """
         return self.dal.get_all() 
 
+
+    def get_by_customer(self, user_id):
+        pass
+    #get all tickets of a specific customer
