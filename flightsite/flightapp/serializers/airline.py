eb9963ef3e4c19f860c12bd9d66ba32fbd860440
sapir mamann <sapir1999@outlook.com>
2023-08-12 23:55:48 +0300

    Needs airline country serialized

--- flightsite/flightapp/serializers/airline.py
+++ flightsite/flightapp/serializers/airline.py
@@ -4,6 +4,9 @@ from ..models import AirlineCompany
 
 
 class AirlineCompanySerializer(serializers.ModelSerializer):
+    # Used to display the country name instead of the country id
+    # country = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
     class Meta:
         model = AirlineCompany
         fields = '__all__' #('name','id',)     #delete id 
